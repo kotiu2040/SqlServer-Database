3a79bde17444147fcbba6f30478e67843ef8798b
kotiu2040 <95004107+kotiu2040@users.noreply.github.com>
2022-10-09 08:13:36 +0400

    Update Product.sql

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,6 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL,
     [Brand] VARCHAR(50) NULL
 )
